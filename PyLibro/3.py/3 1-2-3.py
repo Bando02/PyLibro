4abbeee2de518c0762d65262980cbe1ea973a707
fvp02 <152099617+Bando02@users.noreply.github.com>
2024-03-15 19:22:32 +0100

    Add new File
    New file called 3 1-2-3

---
+++ PyLibro/3.py/3 1-2-3.py
@@ -0,0 +1,26 @@
+#Nombres 
+
+#Ejercicio 3-1
+
+#lista con los nombres
+nombres = ['Dani','Fernando','Armando','Wanan']
+
+#posicion de cada nombre
+print(nombres[0])
+print(nombres[1])
+print(nombres[2])
+print(nombres[3])
+
+
+
+#Ejercicio 3-2
+
+#Funcion f para lectura de variables más llaves para acceder a posiciones de la lista
+print(f"El {nombres[0]} no para de hablar con {nombres[3]}")
+
+#Ejercicio 3-3
+
+motos = ["honda","kawasaki","suzuki","bmw"]
+ruedas = ["tipoA","tipoB","tipoC"]
+
+print(f"Me gustaria tener una moto {motos[1]} con ruedas {ruedas[0]}")
